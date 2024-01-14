19e574291931ed194053a566584e7a1d479d7452
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-01-14 12:49:29 +0100

    Update map icon in routes.js

--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,7 @@ var routes = [
     path: '/map',
     name: 'Map',
     rtlName: 'خرائط',
-    icon: 'tim-icons icon-pin',
+    icon: 'tim-icons icon-map-big',
     component: <Map />,
     layout: '/admin',
   },
