64d9220f78ed5a11d8c6e97f6f0c5db8401ccf48
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-01-15 09:18:56 +0100

    Add DataGrid component and import it in DetailSidebar

--- src/components/DetailSidebar/DetailSidebar.js
+++ src/components/DetailSidebar/DetailSidebar.js
@@ -6,6 +6,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
+import BasicExampleDataGrid from 'components/DataGrid/DataGrid';
 
 const useStyles = makeStyles({
   drawer: {
@@ -26,6 +27,7 @@ function DetailSidebar({ open, onClose, info }) {
       variant="persistent"
       className={classes.drawer}
     >
+      <BasicExampleDataGrid></BasicExampleDataGrid>
       <List>
         <ListItem button onClick={onClose}>
           <ListItemText primary="Close" />
