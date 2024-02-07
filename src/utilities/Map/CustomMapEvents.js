67219012ac838427fc14a1e19259cbac2c9bbc46
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-02-07 20:12:32 +0100

    Add papaparse library for CSV parsing, estimated height

--- src/utilities/Map/CustomMapEvents.js
+++ src/utilities/Map/CustomMapEvents.js
@@ -3,6 +3,8 @@ import L from 'leaflet';
 import PropTypes from 'prop-types';
 import { WMSGetFeatureInfo } from 'ol/format';
 import { nibioGetFeatInfoBaseParams } from 'variables/forest';
+import { useEffect, useState } from 'react';
+import Papa from 'papaparse';
 
 CustomMapEvents.propTypes = {
   activeOverlay: PropTypes.shape({
@@ -13,7 +15,6 @@ CustomMapEvents.propTypes = {
   }).isRequired,
   setActiveOverlay: PropTypes.func.isRequired,
   setActiveFeature: PropTypes.func.isRequired,
-  handleSkogbrukWMSFeatures: PropTypes.func.isRequired,
   hideLayerControlLabel: PropTypes.func.isRequired,
 };
 
@@ -23,6 +24,40 @@ export default function CustomMapEvents({
   setActiveFeature,
   hideLayerControlLabel,
 }) {
+  const [granCSVData, setGranCSVData] = useState([]);
+  const [furuCSVData, setFuruCSVData] = useState([]);
+
+  useEffect(() => {
+    // URL to your CSV file, can be a local static file or a remote resource
+    const granCSVFileUrl = '/csvs/gran.csv';
+    const furuCSVFileUrl = '/csvs/furu.csv';
+
+    async function fetchData(filepath, setData) {
+      const response = await fetch(filepath);
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw stream
+      const decoder = new TextDecoder('utf-8');
+      const csv = decoder.decode(result.value); // convert stream to csv text
+      Papa.parse(csv, {
+        complete: function (results) {
+          setData(results.data);
+        },
+        header: true, // Set to true if your CSV has header rows, it will parse them as object keys
+      });
+    }
+
+    const handleGranCSVData = (data) => {
+      setGranCSVData(data);
+    };
+
+    const handleFuruCSVData = (data) => {
+      setFuruCSVData(data);
+    };
+
+    fetchData(granCSVFileUrl, handleGranCSVData);
+    fetchData(furuCSVFileUrl, handleFuruCSVData);
+  }, []); // Empty dependency array means this effect will only run once, after the initial render
+
   const handleSkogbrukWMSFeatures = (e, features, map) => {
     if (features.length > 0 && features[0]) {
       const feature = features[0];
@@ -41,15 +76,57 @@ export default function CustomMapEvents({
       const activeOverlayNames = Object.keys(activeOverlay).filter(
         (key) => activeOverlay[key] === true
       );
-
+      let estimatedHeight;
       let content =
         `<h3 style="color: black; text-align: center;">${activeOverlayNames[0]}</h3>` + // Add the layer name as the title with black color and centered alignment
         '<table style="margin-bottom: 10px; border-collapse: collapse; border: 1px solid black;">'; // Add margin-bottom and border styles
       for (const key in values) {
         if (desiredAttributes[key]) {
-          content += `<tr style="border: 1px solid black;"><td style="padding: 5px; border: 1px solid black;">${desiredAttributes[key]}</td><td style="padding: 5px; border: 1px solid black;">${values[key]}</td></tr>`; // Add padding-right and border styles
+          let value = values[key];
+          if (key === 'bonitet_beskrivelse') {
+            value = value.substring(value.indexOf(' ') + 1); // Remove the first part and keep only the number
+          }
+          content += `<tr style="border: 1px solid black;"><td style="padding: 5px; border: 1px solid black;">${desiredAttributes[key]}</td><td style="padding: 5px; border: 1px solid black;">${value}</td></tr>`; // Add padding-right and border styles
         }
       }
+
+      // Add the additional row if hogstkl_verdi is 4 or 5
+      if (values.hogstkl_verdi === '4' || values.hogstkl_verdi === '5') {
+        if (values.bontre_beskrivelse === 'Gran') {
+          if (granCSVData.length > 0) {
+            const row = granCSVData.find(
+              (row) =>
+                row.H40 ===
+                values.bonitet_beskrivelse.substring(
+                  values.bonitet_beskrivelse.indexOf(' ') + 1
+                )
+            ); // Use the temporary variable in the find function
+            if (row) {
+              console.log(row[values.alder]);
+            }
+          }
+        }
+        if (values.bontre_beskrivelse === 'Furu') {
+          if (furuCSVData.length > 0) {
+            const row = furuCSVData.find(
+              (row) =>
+                row.H40 ===
+                values.bonitet_beskrivelse.substring(
+                  values.bonitet_beskrivelse.indexOf(' ') + 1
+                )
+            ); // Use the temporary variable in the find function
+            if (row) {
+              estimatedHeight = row[values.alder];
+              if (parseInt(values.alder >= 110)) {
+                estimatedHeight = row['110'];
+              }
+            }
+          }
+        }
+        content += `<tr style="border: 1px solid black;"><td style="padding: 5px; border: 1px solid black;">Overhøyde</td><td style="padding: 5px; border: 1px solid black;">${estimatedHeight}</td></tr>`;
+        content += `<tr style="border: 1px solid black;"><td style="padding: 5px; border: 1px solid black;">Volum av tømmer i bestand</td><td style="padding: 5px; border: 1px solid black;">TBD</td></tr>`;
+      }
+
       content += '</table>';
 
       L.popup().setLatLng(e.latlng).setContent(content).openOn(map);
