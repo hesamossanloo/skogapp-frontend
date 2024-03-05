1107dd014279c59535410344724d7b8e2f90171d
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-03-05 12:40:00 +0100

    Ignore React import ESLINT rule because of v17+, organize imports

--- src/index.js
+++ src/index.js
@@ -15,20 +15,19 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import AdminLayout from 'layouts/Admin/Admin.js';
 import RTLLayout from 'layouts/RTL/RTL.js';
 
-import 'assets/scss/black-dashboard-react.scss';
-import 'assets/demo/demo.css';
-import 'assets/css/nucleo-icons.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'assets/css/nucleo-icons.css';
+import 'assets/demo/demo.css';
+import 'assets/scss/black-dashboard-react.scss';
 
-import ThemeContextWrapper from './components/ThemeWrapper/ThemeWrapper';
 import BackgroundColorWrapper from './components/BackgroundColorWrapper/BackgroundColorWrapper';
+import ThemeContextWrapper from './components/ThemeWrapper/ThemeWrapper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -39,10 +38,7 @@ root.render(
         <Routes>
           <Route path="/admin/*" element={<AdminLayout />} />
           <Route path="/rtl/*" element={<RTLLayout />} />
-          <Route
-            path="*"
-            element={<Navigate to="/admin/dashboard" replace />}
-          />
+          <Route path="*" element={<Navigate to="/admin/map" replace />} />
         </Routes>
       </BrowserRouter>
     </BackgroundColorWrapper>
--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -16,15 +16,15 @@
 
 */
 import React from 'react';
-import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 // javascript plugin used to create scrollbars on windows
 import PerfectScrollbar from 'perfect-scrollbar';
 
 // core components
-import AdminNavbar from 'components/Navbars/AdminNavbar.js';
-import Footer from 'components/Footer/Footer.js';
-import Sidebar from 'components/Sidebar/Sidebar.js';
 import FixedPlugin from 'components/FixedPlugin/FixedPlugin.js';
+import Footer from 'components/Footer/Footer.js';
+import AdminNavbar from 'components/Navbars/AdminNavbar.js';
+import Sidebar from 'components/Sidebar/Sidebar.js';
 
 import routes from 'routes.js';
 
@@ -126,7 +126,7 @@ function Admin(props) {
               </Routes>
               {
                 // we don't want the Footer to be rendered on map page
-                location.pathname === '/admin/maps' ? null : <Footer fluid />
+                location.pathname === '/admin/map' ? null : <Footer fluid />
               }
             </div>
           </div>
