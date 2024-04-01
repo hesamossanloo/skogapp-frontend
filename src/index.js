24a997e00a26874a4c64da3baf20a7a43b7cf3d7
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-04-01 15:35:39 +0100

    Remove unused components and routes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import AdminLayout from 'layouts/Admin/Admin.js';
-import RTLLayout from 'layouts/RTL/RTL.js';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'assets/css/nucleo-icons.css';
@@ -37,7 +36,6 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/admin/*" element={<AdminLayout />} />
-          <Route path="/rtl/*" element={<RTLLayout />} />
           <Route path="*" element={<Navigate to="/admin/map" replace />} />
         </Routes>
       </BrowserRouter>
