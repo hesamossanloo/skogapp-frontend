85dcbba1d44b61da2f731b776a7a5dd7689a862c
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-05-08 21:54:34 +0300

    Update sidebar component to use useEffect hook and add defaultOpen prop to Accordion component, add V from CSV, show V and Worth on Sidebar for HK5 and 4

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,7 +1,7 @@
 import ToggleSwitch from 'components/ToggleSwitch/ToggleSwitch';
 import { MapFilterContext } from 'contexts/MapFilterContext';
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import {
   Button,
   Card,
@@ -9,6 +9,8 @@ import {
   Label,
   UncontrolledCollapse,
 } from 'reactstrap';
+import useCsvData from 'utilities/Map/useCSVData';
+import { CSV_URLS, SPECIES, SPECIES_PRICES } from 'variables/forest';
 
 const cardStyle = {
   background: 'transparent',
@@ -18,23 +20,58 @@ const cardStyle = {
 const labelStyle = {
   fontSize: '0.80rem',
 };
-
-const Accordion = ({ onChange }) => {
+const Accordion = ({ onChange, defaultOpen }) => {
+  const featureInfosData = useCsvData(CSV_URLS.FEATUREINFOS).data;
   const [mapFilter] = useContext(MapFilterContext);
+  const [volume, setVolume] = useState(0);
+  const [worth, setWorth] = useState(0);
+
+  // On HK5 change, go through the featureInfosData and find the rows where the hogstkl_verdi is 5
+  // Then, get the sum of the values under the column Volume
+  const calculateVolume = () => {
+    let sumV = 0;
+    let sumWorth = 0;
+    featureInfosData.forEach((row) => {
+      const rowV = parseFloat(row.Volume) || 0;
+      if (mapFilter.HK5 && row.hogstkl_verdi === '5') {
+        sumV += rowV;
+        if (row.bontre_beskrivelse === 'Bjørk / lauv') {
+          sumWorth += rowV * (SPECIES_PRICES.LAU || 0);
+        } else {
+          sumWorth +=
+            rowV * (SPECIES_PRICES[row.bontre_beskrivelse.toUpperCase()] || 0);
+        }
+      }
+      if (mapFilter.HK4 && row.hogstkl_verdi === '4') {
+        sumV += rowV;
+        if (row.bontre_beskrivelse === 'Bjørk / lauv') {
+          sumWorth += rowV * (SPECIES_PRICES.LAU || 0);
+        } else {
+          sumWorth +=
+            rowV * (SPECIES_PRICES[row.bontre_beskrivelse.toUpperCase()] || 0);
+        }
+      }
+    });
+    return [sumV, sumWorth];
+  };
+
+  // whenever the mapFilter.HK5 changes, update the mapFilter.V
+
+  useEffect(() => {
+    const [sumV, sumWorth] = calculateVolume();
+    setVolume(sumV);
+    setWorth(sumWorth);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mapFilter]);
   return (
     <>
       <Button color="warning" href="#mapFilter" id="linkToggler">
         FILTER
       </Button>
-      <UncontrolledCollapse toggler="#linkToggler" defaultOpen>
+      <UncontrolledCollapse toggler="#linkToggler" defaultOpen={defaultOpen}>
         <Card style={cardStyle}>
           <CardBody>
-            <div
-              style={{ display: 'flex', justifyContent: 'space-between' }}
-            ></div>
-            <div
-              style={{ display: 'flex', justifyContent: 'space-between' }}
-            ></div>
+            {/* HK5 */}
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <Label style={labelStyle}>HK5</Label>
               <ToggleSwitch
@@ -50,6 +87,7 @@ const Accordion = ({ onChange }) => {
                 }
               />
             </div>
+            {/* HK4 */}
             <div
               style={{
                 display: 'flex',
@@ -71,6 +109,7 @@ const Accordion = ({ onChange }) => {
                 }
               />
             </div>
+            {/* Protected */}
             <div
               style={{
                 display: 'flex',
@@ -92,6 +131,79 @@ const Accordion = ({ onChange }) => {
                 }
               />
             </div>
+            {/* Summaries */}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 30,
+              }}
+            >
+              <Label style={labelStyle}>
+                <u>Summary</u>
+              </Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}
+            >
+              <Label style={labelStyle}>Total Volume</Label>
+              <Label style={labelStyle}>{Math.ceil(volume)} m3</Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}
+            >
+              <Label style={labelStyle}>Total Worth</Label>
+              <Label style={labelStyle}>{Math.ceil(worth)} kr</Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 30,
+              }}
+            >
+              <Label style={labelStyle}>
+                <u>Species Prices</u>
+              </Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}
+            >
+              <Label style={labelStyle}>{SPECIES.GRAN}</Label>
+              <Label style={labelStyle}>{SPECIES_PRICES.GRAN} kr</Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}
+            >
+              <Label style={labelStyle}>{SPECIES.FURU}</Label>
+              <Label style={labelStyle}>{SPECIES_PRICES.FURU} kr</Label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}
+            >
+              <Label style={labelStyle}>{SPECIES.LAU}</Label>
+              <Label style={labelStyle}>{SPECIES_PRICES.LAU} kr</Label>
+            </div>
           </CardBody>
         </Card>
       </UncontrolledCollapse>
@@ -108,6 +220,7 @@ const Accordion = ({ onChange }) => {
 
 Accordion.propTypes = {
   onChange: PropTypes.func.isRequired,
+  defaultOpen: PropTypes.bool.isRequired,
 };
 
 export default Accordion;
